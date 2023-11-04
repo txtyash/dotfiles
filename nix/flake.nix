ed44902d90d47ce3f7c664fa5d64a439d5734985
yash shinde <evccyr@proton.me>
2023-11-04 17:44:56 +0530

    add china cache link

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,6 +3,19 @@
 
   nixConfig = {
     experimental-features = ["nix-command" "flakes"];
+    substituters = [
+      # Replace the official cache with a mirror located in China
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+    ];
+
+    extra-substituters = [
+      # Nix community's cache server
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   inputs = {
@@ -27,7 +40,6 @@
           system = "x86_64-linux";
           specialArgs = {
             inherit inputs;
-            profile = "yash";
           };
           modules = [
             # Machine configuration
