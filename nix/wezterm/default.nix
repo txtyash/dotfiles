c01f04f837af70cfff712e72b14316c55416933f
yash shinde <evccyr@proton.me>
2023-11-10 23:08:57 +0530

    customize shortcuts

--- nix/wezterm/default.nix
+++ nix/wezterm/default.nix
@@ -1,51 +1,23 @@
-{config, ...}: let
-  inherit (config) colorScheme;
-  inherit (colorScheme) colors;
+{...}: let
 in {
   programs.wezterm = {
     enable = true;
-    # colorSchemes = {
-    #   "${colorScheme.slug}" = {
-    #     foreground = "#${colors.base05}";
-    #     background = "#${colors.base00}";
-    #
-    #     ansi = [
-    #       "#${colors.base08}"
-    #       "#${colors.base09}"
-    #       "#${colors.base0A}"
-    #       "#${colors.base0B}"
-    #       "#${colors.base0C}"
-    #       "#${colors.base0D}"
-    #       "#${colors.base0E}"
-    #       "#${colors.base0F}"
-    #     ];
-    #     brights = [
-    #       "#${colors.base00}"
-    #       "#${colors.base01}"
-    #       "#${colors.base02}"
-    #       "#${colors.base03}"
-    #       "#${colors.base04}"
-    #       "#${colors.base05}"
-    #       "#${colors.base06}"
-    #       "#${colors.base07}"
-    #     ];
-    #     cursor_fg = "#${colors.base00}";
-    #     cursor_bg = "#${colors.base05}";
-    #     selection_fg = "#${colors.base00}";
-    #     selection_bg = "#${colors.base05}";
-    #   };
-    # };
-    # -- font = wezterm.font("${config.fontProfiles.monospace.family}"),
     # Contents for wezterm.lua
-    #   extraConfig = ''
-    #       return {
-    #         font_size = 12.0,
-    #         hide_tab_bar_if_only_one_tab = true,
-    #         window_close_confirmation = "NeverPrompt",
-    #         set_environment_variables = {
-    #           TERM = 'wezterm',
-    #         },
-    #       }
-    #     '';
+    extraConfig = ''
+      return {
+        font_size = 12.0,
+        hide_tab_bar_if_only_one_tab = true,
+        window_close_confirmation = "NeverPrompt",
+        set_environment_variables = {
+          TERM = 'wezterm',
+        },
+        keys = {
+          { key = 'h', mods = 'ALT', action = wezterm.action.ActivateTabRelative(-1) },
+          { key = 'l', mods = 'ALT', action = wezterm.action.ActivateTabRelative(1) },
+          { key = 'w', mods = 'ALT', action = wezterm.action.CloseCurrentTab { confirm = false }, },
+          { key = 't', mods = 'ALT', action = wezterm.action.SpawnTab 'CurrentPaneDomain', },
+        },
+      }
+    '';
   };
 }
