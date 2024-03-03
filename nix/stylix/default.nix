1210eb4aa7ed56b66ec2724f59b274204105cc3e
Yash Shinde <shinde27yash@gmail.com>
2024-03-04 01:43:49 +0530

    Change wallpaper

--- nix/stylix/default.nix
+++ nix/stylix/default.nix
@@ -2,7 +2,7 @@
   stylix =
     {
       cursor.package = pkgs.catppuccin-cursors;
-      image = ../../Pictures/gruv-pixel.png;
+      image = ../../Pictures/gruv-room.png;
       base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-light-medium.yaml";
       cursor.name = "macchiatoFlamingo";
       polarity = "light";
