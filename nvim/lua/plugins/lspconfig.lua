f2b6c6c741cf410959ecd6beb8d73a175dc98740
textyash <codeberg@textyash.com>
2026-01-30 12:20:18 +0530

    enable harper only for select file types

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
+
     vim.lsp.enable({
       -- note taking
       "tinymist",
@@ -22,5 +23,11 @@ return {
       "ts_ls",
       "qmlls"
     })
+
+    -- Enable Harper LSP for only select file types
+    local harper_cfg = vim.lsp.config.harper_ls
+    harper_cfg.filetypes = { "asciidoc", "gitcommit", "html", "markdown", "toml", "yaml", "typst", "text" }
+    vim.lsp.config("harper_ls", harper_cfg)
+
   end,
 }
