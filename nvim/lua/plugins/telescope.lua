d61bac629da1c32764359faef4d989b8d69bed47
txtyash <github@textyash.com>
2026-04-09 18:39:25 +0530

    resolve neovim issues after 0.12 update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.2.0",
+  version = '*',
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local map = vim.keymap.set
