a2d2d91cfc9ce702a9d76a03085f62ce191f754a
txtyash <github@textyash.com>
2026-04-20 20:19:03 +0530

    fix nixos does not boot due to incorrect uuid in vivobook.nix

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelModules = [ "i2c-dev" ];
-  boot.kernelPackages = pkgs.linuxPackages_testing;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.consoleLogLevel = 0;
   boot.initrd.verbose = false;
   boot.kernelParams = [
