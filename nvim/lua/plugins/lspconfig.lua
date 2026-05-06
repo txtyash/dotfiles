bb9397471d0aeaec559ab2e9acaf26a4450919b3
txtyash <github@textyash.com>
2026-05-07 00:41:20 +0530

    add lsp key bindings to neovim

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -26,6 +26,16 @@ return {
       "gopls"
     })
 
+    -- LSP keymaps
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(ev)
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
+      end,
+    })
+
     -- Enable Harper LSP for only select file types
     local harper_cfg = vim.lsp.config.harper_ls
     harper_cfg.filetypes = { "asciidoc", "gitcommit", "html", "markdown", "toml", "yaml", "typst", "text" }
